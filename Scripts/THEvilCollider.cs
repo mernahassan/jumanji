5dc7f5b0291214441dc553c6932d040d12cc0ab6
mernahassan <mernahassan96@gmail.com>
2018-04-28 19:25:33 +0200

    add new script

---
+++ Scripts/THEvilCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class THEvilCollider : MonoBehaviour {
+
+    public int Damage;
+    public THEvilController owner;
+    void OnTriggerEnter(Collider col)
+    {
+        stefaniController somebody = col.gameObject.GetComponent<stefaniController>();
+
+        if (owner.state == "attacking" && somebody != null)
+        {
+            stefHealth health = somebody.GetComponent<stefHealth>();
+            somebody.movementControl("hit_head");
+            if (health != null)
+            {
+                health.TakeDamage(Damage);
+            }
+
+        }
+
+    }
+}
